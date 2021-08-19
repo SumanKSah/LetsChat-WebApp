9ee00f3fa74d5231ece96fe10237a7ed03b0bb0d
Suman Kumar Sah <sahsumankumar3@gmail.com>
2021-08-19 21:48:37 +0530

    Adding socket.io library and username and password field

---
+++ public/index.js
@@ -0,0 +1,23 @@
+//NavBar
+const user = document.getElementById("mainUser");
+const btnLogin = document.getElementById("signIn");
+const btnSignup = document.getElementById("signUp");
+const loginUser = document.getElementById("loginUser");
+const loginPassword = document.getElementById("loginPassword");
+
+//chatBox
+const inpMsg = document.getElementById("messageBox");
+const btnsendMsg = document.getElementById("sendMessage");
+const chatList = document.getElementById("scrl");
+chatList.scrollTop = chatList.scrollHeight;
+
+// sideNav bar elements
+const connectedUser = document.getElementById("userName");
+const btnNext = document.getElementById("nextUser");
+const availableUser = document.getElementById("availableUser");
+const onlineUser = document.getElementById("totalOnline");
+
+
+
+btnsendMsg.disabled = true;
+inpMsg.disabled = true;
--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use('/', express.static(__dirname + '/public'))
 const http = require('http')
 const server = http.createServer(app)
 
+const socketio = require('socket.io')
+const io = socketio(server)
+
+
+io.on('connection',(socket)=>{
+    console.log('user connected with id: ',socket);
+})
 
 server.listen(8000, ()=>{
     console.log('Server started at http://localhost:8000');
